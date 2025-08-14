28a038e91924097e4808c85d89d0a9608bd92b2d
neroaccff1 <neroaccff1@gmail.com>
2025-08-14 23:13:45 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
   email: "pramithalakshana525@gmail.com",
-  password: "pramitha_2011",
+  password: "pramitha_20112",
   userAgent:
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
 };
@@ -41,4 +41,5 @@ const upload = (data, name) => {
 };
 
 module.exports = { upload };
+
 
